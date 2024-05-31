b1873071ba64cbf0c263d75e64429df75901c83f
cleitonbrandao <cbs3dmax@gmail.com>
2024-05-31 15:08:53 -0300

    Lista de Serviços

--- app/Livewire/Service/HomeService.php
+++ app/Livewire/Service/HomeService.php
@@ -3,13 +3,13 @@
 namespace App\Livewire\Service;
 
 use Livewire\Component;
-//use App\Models\Service;
+use App\Models\Service;
 class HomeService extends Component
 {
     public function home()
     {
         return view('livewire.service.index',
-//            ['services' => Services::all()]
+            ['services' => Service::all()]
         );
     }
 }
