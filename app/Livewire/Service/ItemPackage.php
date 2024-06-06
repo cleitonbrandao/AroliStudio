5887cde795440a7a8f0eb9ada1740a6bf5104752
cleitonbrandao <cbs3dmax@gmail.com>
2024-06-06 16:09:37 -0300

    ajustes na forma como se trata items de pacote

--- app/Livewire/Service/ItemPackage.php
+++ app/Livewire/Service/ItemPackage.php
@@ -2,30 +2,28 @@
 
 namespace App\Livewire\Service;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ItemPackage extends Component
 {
-    public array $packages_items = [];
+    public mixed $packages_items = [];
     #[On('items-update')]
     public function render()
     {
-        $products = Cache::get('packages_items.products', []);
-        $services = Cache::get('packages_items.services', []);
-        $this->packages_items = [$products, $services];
-//        dd($packages_items);
+        $this->packages_items = Cache::get('packages_items', []);
         return view('livewire.service.item-package');
     }
     public function removeItem($item)
     {
-        $items = cache('items');
+        $items = cache('packages_items');
         if (isset($items[$item]))
         {
-            unset($items[$item]);
-            $items = array_values($items);
-            Cache::put('items', $items);
+            Arr::forget($items, $item);
+            Cache::put('packages_items', array_values($items));
+            $this->dispatch('remove-items-update');
         }
     }
 }
