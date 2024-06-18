d372aa38f1758bf2bf9fb880d756a139985d0b55
cleitonbrandao <cbs3dmax@gmail.com>
2024-06-18 18:06:10 -0300

    correção do carregamento de modal-form-product

--- resources/views/livewire/service/products-pagination.blade.php
+++ resources/views/livewire/service/products-pagination.blade.php
@@ -21,7 +21,7 @@
             <tbody>
             @foreach($products as $product)
                 <x-modal name="product-modal-{{ $product->id }}" id="{{ $product->id }}">
-                        testando
+                        @include('livewire.service.modal-form-product')
                 </x-modal>
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
